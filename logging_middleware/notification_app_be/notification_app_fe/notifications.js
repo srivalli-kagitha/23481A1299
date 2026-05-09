b3a51e11a9006a8fbb885d88563c6f12dbf809a9
Srivalli Kagitha <srivallikagitha01@gmail.com>
2026-05-09 13:12:28 +0530

    Add files via upload

---
+++ logging_middleware/notification_app_be/notification_app_fe/notifications.js
@@ -0,0 +1,32 @@
+const notifications = [
+  {
+    id: 1,
+    title: "Exam Schedule Released",
+    type: "Academic",
+    priority: 1,
+    viewed: false,
+  },
+  {
+    id: 2,
+    title: "Placement Drive Announcement",
+    type: "Placement",
+    priority: 2,
+    viewed: true,
+  },
+  {
+    id: 3,
+    title: "Holiday Notice",
+    type: "General",
+    priority: 3,
+    viewed: false,
+  },
+  {
+    id: 4,
+    title: "Workshop Registration",
+    type: "Event",
+    priority: 1,
+    viewed: true,
+  },
+];
+
+export default notifications;
